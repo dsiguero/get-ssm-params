db3af35fbfd04fe4f33427c5f416affeb59d8d85
Daniel Siguero <daniel.siguero@gmail.com>
2018-04-30 23:12:22 +0100

    - Slightly better error handling - No commented code - Slightly better logging

--- src/json.go
+++ src/json.go
@@ -5,8 +5,6 @@ import (
   "fmt"
   "os"
   "errors"
-
-//"io/ioutil"
   "io/ioutil"
 )
 
@@ -14,45 +12,37 @@ func main() {
   args := os.Args[1:]
 
   if len(args) < 1 || len(args) > 1 {
-    panic(errors.New("You need to provide a JSON file (key-value, flat) as the only argument."))
+    panic(errors.New("you need to provide a JSON file (key-value, flat) as the only argument"))
   }
 
   jsonFile := args[0]
-  fmt.Println(jsonFile)
-
   pages := readJsonFile(jsonFile)
-  //fmt.Println(pages)
 
   for k, p := range pages {
     fmt.Printf("%s: %s\n" , k, p)
   }
-
-
-  //byt := []byte(`{"key1":"test","key2":"test2"}`)
-  //
-  //var dat map[string]interface{}
-  //
-  //if err := json.Unmarshal(byt, &dat); err != nil {
-  //  panic(err)
-  //}
-  //fmt.Println(dat)
 }
 
-func readJsonFile(filePath string)map[string]interface{} {
+func readJsonFile(filePath string) map[string]interface{} {
   raw, err := ioutil.ReadFile(filePath)
 
   if err != nil {
     panic(err)
+  } else {
+    fmt.Printf("Using `%s` as input file.\n\n", filePath)
   }
 
   var c map[string]interface{}
-  json.Unmarshal(raw, &c)
+  if err := json.Unmarshal(raw, &c); err != nil {
+    panic(err)
+  }
+
   return c
 }
 
 func panic(e error) {
   if e != nil {
-    fmt.Fprintf(os.Stderr, "ERROR: %v\n", e)
+    fmt.Fprintf(os.Stderr, "ERROR: %v.\n", e)
     os.Exit(1)
   }
 }
